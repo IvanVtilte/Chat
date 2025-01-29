2f7d7602f2cd5098fae2eb227a4c5702a33f5199
IvanVtilte <medvedovskijivan@gmail.com>
2025-01-29 23:14:27 +0300

    Update User.h

--- Chat/User.h
+++ Chat/User.h
@@ -8,12 +8,10 @@ public:
     std::string username;
     std::string password;
     std::string name;
-
-    // Конструктор
     User(std::string uname, std::string pass, std::string fullname)
-        : username(uname), password(pass), name(fullname) {}
-
-    // Вывод информации о пользователе
+        : username(uname), password(pass), name(fullname) {
+    }
+    // Р’С‹РІРѕРґ РёРЅС„РѕСЂРјР°С†РёРё Рѕ РїРѕР»СЊР·РѕРІР°С‚РµР»Рµ
     void display() const {
         std::cout << name << " (" << username << ")" << std::endl;
     }
