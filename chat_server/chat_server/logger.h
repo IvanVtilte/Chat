ebaaea7d635c67bf4d9383fe2d2746785a4f6349
IvanVtilte <medvedovskijivan@gmail.com>
2025-05-26 14:03:55 +0300

    Update logger.h

--- chat_server/chat_server/logger.h
+++ chat_server/chat_server/logger.h
@@ -1,1 +1,27 @@
+#ifndef LOGGER_H
+#define LOGGER_H
 
+#include <string>
+#include <fstream>
+#include <shared_mutex>
+#include <chrono>
+#include <iomanip>
+#include <sstream>
+
+class Logger {
+private:
+    std::fstream log_file;
+    mutable std::shared_mutex file_mutex;
+    std::string get_current_time();
+
+public:
+    Logger(const std::string& filename);
+    ~Logger();
+    void write_log(const std::string& message);
+    std::string read_log();
+
+    Logger(const Logger&) = delete;
+    Logger& operator=(const Logger&) = delete;
+};
+
+#endif 
